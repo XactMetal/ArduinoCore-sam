466fbbf44169585d62b730bc45a93d2c12d1f973
Chandler Griscom <cjgriscom@gmail.com>
2025-11-25 15:47:01 -0500

    Reduce I2C timeout

--- libraries/Wire/src/Wire.h
+++ libraries/Wire/src/Wire.h
@@ -112,8 +112,8 @@ private:
 	uint32_t twiClock;
 
 	// Timeouts (
-	static const uint32_t RECV_TIMEOUT = 100000;
-	static const uint32_t XMIT_TIMEOUT = 100000;
+	static const uint32_t RECV_TIMEOUT = 20000;
+	static const uint32_t XMIT_TIMEOUT = 20000;
 };
 
 #if WIRE_INTERFACES_COUNT > 0
